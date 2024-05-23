53ca2a27cdba133ca4471abdddb3bf5dcd04389b
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-05-23 20:38:13 +0300

    Update 7.ts

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -4,18 +4,18 @@
 */
 
 export enum UserRole {
-  admin = 'admin',
-  editor = 'editor',
-  guest = 'guest',
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
 }
 
 // Замініть наступний код на версію за допомогою Record
 
-type RoleDescript = Record<UserRole, string>
+type RoleDescript = Record<UserRole, string>;
 const RoleDescription: RoleDescript = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+  admin: "Admin User",
+  editor: "Editor User",
+  guest: "Guest User",
 };
 
 export {};
